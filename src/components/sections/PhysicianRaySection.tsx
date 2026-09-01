aa6ce18a54fd493ef46a3f2b6de6487295e5d3bd
Bigyan Poudel <bgyanpoudel10@gmail.com>
2026-09-01 21:17:27 +0545

    fix: git

--- src/components/sections/PhysicianRaySection.tsx
+++ src/components/sections/PhysicianRaySection.tsx
@@ -25,6 +25,7 @@ export default function PhysicianRaySection() {
   const sectionRef = useRef<HTMLElement>(null);
 
 
+
   useGSAP(
     () => {
       gsap.utils.toArray<HTMLElement>("[data-reveal]").forEach((el) => {
